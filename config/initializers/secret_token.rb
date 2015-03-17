b0b77d65d519d13f4110f7a38674dcc5dcb71a3e
jishana <jishana@carmatec.com>
2015-03-17 11:24:45 +0530

    simply

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jishana::Application.config.secret_token = '5d5d622214d93957991e4b34035f264235bd7f320adf5761870edd2bbbe4b91be5399c706c22eaaf596121e0b5697048de34748381c9dc7287e99544606e7481'
